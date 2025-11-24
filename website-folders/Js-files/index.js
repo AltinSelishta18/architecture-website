57016007c7b7ceab3514141ef75018c01a0f4cc1
Altin Selishta <AltinSelishta14@gmail.com>
2025-11-25 00:25:57 +0100

    Some features added to navigation elements and header-footer-left design finished

--- website-folders/Js-files/index.js
+++ website-folders/Js-files/index.js
@@ -1,1 +1,11 @@
 //JS code dedicated for index.html file
+
+// Header footer text changing animation
+
+const texts = ["Crafting Modern Architecture.", "Designing Innovative Spaces", "Shaping Futuristic Environments"]
+let index = 0;
+
+setInterval(() =>{
+    index = (index + 1) % texts.length;
+    document.querySelector("#text").innerText = texts[index];
+}, 3000);
